30496fc16f2595016ec9eb40a42f9cdf192bbfc2
TangMo <win32.exe@anthro.ltd>
2021-04-04 01:08:50 +0100

    Add link to github

--- owo-gen.js
+++ owo-gen.js
@@ -1,30 +1,32 @@
-'use strict';
+'use strict'
 
-const owo = require('@zuzak/owo');
-const fs = require('fs');
+const owo = require('@zuzak/owo')
+const fs = require('fs')
 
-const en = JSON.parse(fs.readFileSync(__dirname + '/src/intl/en.json', 'utf8'));
+const en = JSON.parse(fs.readFileSync(__dirname + '/src/intl/en.json', 'utf8'))
+const previouslyTranslated = JSON.parse(fs.readFileSync(__dirname + '/src/intl/en.json', 'utf8'))
 
 console.log('Generating uwu language...')
 
 const override = {
-    'Telegram': 'Furrygram'
+  Telegram: 'Furrygram',
 }
 
-const recurseOwo = (obj) => {
+const recurseOwo = (obj, previous) => {
   if (typeof obj === 'string') {
-      if (obj in override) {
-          return override[obj];
-      }
-    return owo.translate(obj);
+    if (typeof previous === 'string') {
+      return previous
+    }
+    if (obj in override) {
+      return override[obj]
+    }
+    return owo.translate(obj)
   }
-  Object.keys(obj).map(function(key, index) {
-    obj[key] = recurseOwo(obj[key]);
-  });
-  return obj;
+  Object.keys(obj).map(function (key, index) {
+    obj[key] = recurseOwo(obj[key], previous?.[key])
+  })
+  return obj
 }
-recurseOwo(en)
-en['footer']['note'] = ' - Translated by {uwuifier}';
-console.log(en);
+recurseOwo(en, previouslyTranslated)
 
-fs.writeFileSync(__dirname + '/src/intl/en-emodeng.json', JSON.stringify(en, null, 4));
+fs.writeFileSync(__dirname + '/src/intl/en-emodeng.json', JSON.stringify(en, null, 4))
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react'
-import { Container } from "react-bootstrap"
-import { FormattedMessage } from "gatsby-plugin-intl"
+import { Col, Container, Row } from 'react-bootstrap'
+import { FormattedMessage } from 'gatsby-plugin-intl'
 
 export function Footer() {
-  return (
-    <Container>
-      <hr style={{marginTop: '4rem'}}/>
-      <p>©2021<FormattedMessage id="footer.note" values={{'uwuifier': <a href="https://www.npmjs.com/package/@zuzak/owo" rel="noreferrer noopener">@zuzak/owo</a>}}/></p>
-    </Container>
-  )
+    return (
+        <Container>
+            <hr style={{ marginTop: '4rem' }} />
+            <Row>
+                <Col xs={8}>
+                    <p>
+                        <small>
+                            ©2021
+                            <FormattedMessage
+                                id="footer.note"
+                                values={{
+                                    uwuifier: (
+                                        <a href="https://www.npmjs.com/package/@zuzak/owo" rel="noreferrer noopener">
+                                            @zuzak/owo
+                                        </a>
+                                    ),
+                                }}
+                            />
+                        </small>
+                    </p>
+                </Col>
+                <Col xs={4} className={['text-right']}>
+                    <small>
+                        <a href="https://github.com/tangmo-anthro-ltd/tangmo.anthro.asia" rel="noreferrer noopener">
+                            <FormattedMessage id="footer.source" />
+                        </a>
+                    </small>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
