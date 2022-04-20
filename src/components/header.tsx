4d33310e80dbc1acfd739632ea1d6815030aa6ce
TangMo <tangmo@anthro.ltd>
2022-04-20 04:51:13 +0100

    To bootstrap 5.1.3

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -31,8 +31,8 @@ export function Header({ title, description, extraMeta = [] }: IProps) {
                 {
                     // Bootstrap
                     rel: 'stylesheet',
-                    href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css',
-                    integrity: 'sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn',
+                    href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+                    integrity: 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3',
                     crossOrigin: 'anonymous',
                 },
             ]}
