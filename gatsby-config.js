6230ea40969de79b895e9185606e797dc3906801
TangMo <tangmo@anthro.ltd>
2022-11-28 04:52:25 +0000

    No longer using yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: 'แตงโม',
         short_name: 'แตงโม',
         start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
+        background_color: '#243242',
+        theme_color: '#243242',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
