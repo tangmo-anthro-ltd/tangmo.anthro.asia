9f89dab8d91fbe968904d2db4c8cf5265eabe7d2
TangMo <tangmo@anthro.ltd>
2022-12-03 14:35:18 +0000

    owogen ignore template strings

--- owo-gen.js
+++ owo-gen.js
@@ -4,7 +4,6 @@ const owo = require('@zuzak/owo');
 const fs = require('fs');
 
 const en = JSON.parse(fs.readFileSync(__dirname + '/src/intl/en.json', 'utf8'));
-const previouslyTranslated = JSON.parse(fs.readFileSync(__dirname + '/src/intl/en-emodeng.json', 'utf8'));
 
 console.log('Generating uwu language...');
 
@@ -12,21 +11,34 @@ const override = {
   Telegram: 'Furrygram',
 };
 
-const recurseOwo = (obj, previous) => {
-  if (typeof obj === 'string') {
-    if (typeof previous === 'string') {
-      return previous;
+const owoify = (text) => {
+  // Ignore texts to be substituted such as "Click {link} to learn more"
+  const substrings = text.split(/([{}])/g);
+  let inBraces = false;
+  for (let i = 0; i < substrings.length; i++) {
+    if (substrings[i] === '{') {
+      inBraces = true;
+    } else if (substrings[i] === '}') {
+      inBraces = false;
+    } else if (!inBraces) {
+      substrings[i] = owo.translate(substrings[i]);
     }
+  }
+  return substrings.join('');
+};
+
+const recurseOwo = (obj) => {
+  if (typeof obj === 'string') {
     if (obj in override) {
       return override[obj];
     }
-    return owo.translate(obj);
+    return owoify(obj);
   }
   Object.keys(obj).map(function (key, index) {
-    obj[key] = recurseOwo(obj[key], previous?.[key]);
+    obj[key] = recurseOwo(obj[key]);
   });
   return obj;
 };
-recurseOwo(en, previouslyTranslated);
+recurseOwo(en);
 
 fs.writeFileSync(__dirname + '/src/intl/en-emodeng.json', JSON.stringify(en, null, 4));
