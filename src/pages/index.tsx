4c54959cb5a31790acd6bf01cc2b78fa720824df
TangMo <tangmo@anthro.ltd>
2023-01-04 00:14:20 +0000

    Unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Layout from '../components/layout';
 import { useIntl } from 'gatsby-plugin-react-intl';
 import { ContactSection } from '../components/Pages/HomePage/ContactSection';
